31343eb3f9046c7884d961117c79a19d45447d80
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2022-11-03 19:04:34 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+
+function ButtonIncrement(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className="increment-btn" onClick={props.onClickFunc}>+</button>
+  )
+}
+
+function ButtonDecrement(props) {
+  return (
+    <button className="decrement-btn" onClick={props.onClickFunc}>-</button>
+  )
+}
+
+function Display(props) {
+  return (
+    <label>{props.message}</label>
+  )
+}
+
+
+function App() {
+  const [counter, setCounter] = useState(1);
+  const incrementCounter = () => setCounter(counter + 1);
+  let decrementCounter = () => setCounter(counter - 1);
+
+  if (counter <= 1) {
+    decrementCounter = () => setCounter(1);
+  }
+
+  return (
+    <div className='increment'>
+      <ButtonIncrement onClickFunc={incrementCounter}/>
+      <Display message = {counter}/>
+      <ButtonDecrement onClickFunc={decrementCounter}/>
     </div>
   );
 }
